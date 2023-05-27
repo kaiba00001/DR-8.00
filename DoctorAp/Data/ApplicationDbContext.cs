3577feda3d77179087987692bce66e5b2d70eac4
kaiba00001 <volsonjonah6@gmail.com>
2023-05-27 10:50:48 +0200

    Initial commit

---
+++ DoctorAp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DoctorAp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorAp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BookingLeadEntity> BookingLead { get; set; }
+
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+
+        
+
+
+        public DbSet<DoctorAp.Models.ScreenLead>? ScreenLead_1 { get; set; }
+
+
+        public DbSet<PrescriptionLead> PrescriptionLead { get; set; }
+    }
+}
